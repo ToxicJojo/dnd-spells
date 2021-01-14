ceaccb71e01b300444b323863f2e670cf9349449
Johannes Thiel <Johannes.Thiel@uni-duesseldorf.de>
2021-01-14 16:02:25 +0100

    Spell File (#4)
    * Added a build step to create a spell json file from xml
    
    * Refactored the build step
    
    * Refactored and documented the build process

---
+++ src/types.ts
@@ -0,0 +1,63 @@
+/**
+ * Represents a single spell in DnD 5e.
+ * @see For more information on how spells work in 5e
+ * https://www.dndbeyond.com/sources/basic-rules/spellcasting.
+ */
+export interface Spell {
+  id: string;
+  /**
+   * The name of the spell.
+   */
+  name: string;
+  /**
+   * An array containing all the classes that are able to use this spell.
+   */
+  classes: Array<string>;
+  /**
+   * The spell level of the spell.
+   */
+  level: number;
+  /**
+   * The school of magic the spell belongs to.
+   */
+  school: string;
+  /**
+   * Indicates whether the spell is a ritual or not.
+   */
+  ritual: boolean;
+  /**
+   * The time it takes to cast the spell.
+   */
+  time: string;
+  /**
+   * The range of the spell.
+   */
+  range: string;
+  /**
+   * An array containing the required components of the spell.
+   */
+  components: Array<string>;
+  /**
+   * The duration during which the spells effect last.
+   */
+  duration: string;
+  /**
+   * The rules text of the spell.
+   */
+  text: string;
+  /**
+   * An array containing dice roll associated with the spell.
+   */
+  roll: Array<string>;
+}
+/**
+ * Represents a collection of 5e spells.
+ * Each spell is a property of the spell collection and can be accessed by its id.
+ * @example
+ * const collection: SpellCollection = ...
+ * collection.fireball
+ * collection['acid-splash']
+ */
+export interface SpellCollection {
+  [key: string]: Spell;
+}
