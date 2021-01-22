a61f6183a5cd8cfbc831ca5f5b60de1b3a2001c1
Johannes Thiel <Johannes.Thiel@uni-duesseldorf.de>
2021-01-22 16:55:37 +0100

    Moved the show more/less icons into their own components

--- src/components/SpellSearchResult.vue
+++ src/components/SpellSearchResult.vue
@@ -4,12 +4,8 @@
       .spell-search-result__title
         h2 {{ spell.name }}
         p {{ spell.levelName }} {{ spell.school }}
-      svg(v-if='!showDetails' xmlns='http://www.w3.org/2000/svg' viewbox='0 0 24 24' fill='black' width='18px' height='18px')
-        path(d='M0 0h24v24H0z' fill='none')
-        path(d='M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z')
-      svg(v-else xmlns='http://www.w3.org/2000/svg' viewbox='0 0 24 24' fill='black' width='18px' height='18px')
-        path(d='M0 0h24v24H0z' fill='none')
-        path(d='M12 8l-6 6 1.41 1.41L12 10.83l4.59 4.58L18 14z')
+      ShowMoreIcon(v-if='!showDetails')
+      ShowLessIcon(v-else)
     .spell-search-result__detail(v-if='showDetails')
       .spell-search-result__detail-row
         .spell-search-result__detail-entry
@@ -39,6 +35,8 @@
 <script lang="ts">
 import Vue, { PropType } from 'vue'
 import { Spell } from '@/types'
+import ShowMoreIcon from '@/components/icons/ShowMoreIcon.vue'
+import ShowLessIcon from '@/components/icons/ShowLessIcon.vue'
 
 export default Vue.extend({
   name: 'SpellSearchResult',
@@ -46,6 +44,10 @@ export default Vue.extend({
     return {
       showDetails: false,
     }
+  },
+  components: {
+    ShowMoreIcon,
+    ShowLessIcon,
   },
   props: {
     spell: {
