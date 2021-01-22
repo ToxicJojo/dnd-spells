a7b65e0ab26666daa41ead087683df14860b34a1
Johannes Thiel <Johannes.Thiel@uni-duesseldorf.de>
2021-01-23 00:08:17 +0100

    Moved duplicate markup into SpellInfo component

--- src/components/SpellSearchResult.vue
+++ src/components/SpellSearchResult.vue
@@ -6,30 +6,7 @@
         p {{ spell.levelName }} {{ spell.school }}
       ShowMoreIcon(v-if='!showDetails')
       ShowLessIcon(v-else)
-    .spell-search-result__detail(v-if='showDetails')
-      .spell-search-result__detail-row
-        .spell-search-result__detail-entry
-          strong Casting Time:
-          span(v-if='spell.ritual') {{ spell.time }}/Ritual
-          span(v-else) {{ spell.time }}
-        .spell-search-result__detail-entry
-          strong Range/Area:
-          span {{ spell.range }}
-        .spell-search-result__detail-entry
-          strong Duration:
-          span {{ spell.duration }}
-        .spell-search-result__detail-entry
-          strong Components:
-          span
-            template(v-for='(component, key) in spell.components')
-              template(v-if='key !== 0')
-                | ,
-              | {{ component }}
-      hr
-      p.spell-search-result__text(v-html='spell.text')
-      p(v-if='spell.materialComponent')
-        strong *Material Component:
-        |  {{ spell.materialComponent }}
+    SpellInfo(:spell='spell' v-if='showDetails')
 </template>
 
 <script lang="ts">
@@ -37,6 +14,7 @@ import Vue, { PropType } from 'vue'
 import { Spell } from '@/types'
 import ShowMoreIcon from '@/components/icons/ShowMoreIcon.vue'
 import ShowLessIcon from '@/components/icons/ShowLessIcon.vue'
+import SpellInfo from '@/components/SpellInfo.vue'
 
 export default Vue.extend({
   name: 'SpellSearchResult',
@@ -48,6 +26,7 @@ export default Vue.extend({
   components: {
     ShowMoreIcon,
     ShowLessIcon,
+    SpellInfo,
   },
   props: {
     spell: {
@@ -65,6 +44,7 @@ export default Vue.extend({
   border-radius: 8px;
   width: 100%;
   margin: 16px 0px;
+  padding: 16px 24px;
   background-color: $color-panel-background;
   box-shadow: -1px 4px 10px 0 rgba(0,0,0,.75);
 }
@@ -73,7 +53,6 @@ export default Vue.extend({
   @include flex-row;
   align-items: center;
   cursor: pointer;
-  padding: 16px;
 }
 
 .spell-search-result__title {
@@ -91,43 +70,6 @@ export default Vue.extend({
 
   p {
     margin: 0;
-    color: $color-text-light;
-  }
-}
-
-.spell-search-result__detail {
-  padding: 16px;
-  white-space: pre-line;
-}
-
-.spell-search-result__detail-row {
-  @include flex-row;
-  justify-content: center;
-  flex-wrap: wrap;
-}
-
-.spell-search-result__text {
-  @include flex-col;
-
-  table {
-    display: block;
-    overflow: auto;
-    white-space: pre;
-  }
-}
-
-.spell-search-result__detail-entry {
-  @include flex-col;
-  flex: 50%;
-  align-items: center;
-  text-align: center;
-  padding: 16px;
-
-  @media (min-width: 650px) {
-    flex: 25%;
-  }
-
-  span {
     color: $color-text-light;
   }
 }
